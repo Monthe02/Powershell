3190f1beed273e81e596968e239177930c8a6557
Harri Rundberg <91827712+Monthe02@users.noreply.github.com>
2023-01-10 22:12:26 +0200

    Add files via upload

---
+++ proactive_detection_example.ps1
@@ -0,0 +1,18 @@
+﻿$Path = "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\PanSetup"
+$Name = "Portal"
+$Type = "String"
+$Value = "vpn.company.com"
+
+Try {
+    $Registry = Get-ItemProperty -Path $Path -Name $Name -ErrorAction Stop | Select-Object -ExpandProperty $Name
+    If ($Registry -eq $Value){
+        Write-Output "Compliant"
+        Exit 0
+    } 
+    Write-Warning "Not Compliant"
+    Exit 1
+} 
+Catch {
+    Write-Warning "Not Compliant"
+    Exit 1
+}
---
+++ proactive_remediation_example.ps1
@@ -0,0 +1,11 @@
+﻿if (-not (Test-Path "HKLM:\SOFTWARE\Palo Alto Networks")) {
+  New-Item -Path "HKLM:\SOFTWARE" -Name "Palo Alto Networks" | Out-Null
+}
+if (-not (Test-Path "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect")) {
+  New-Item -Path "HKLM:\SOFTWARE\Palo Alto Networks" -Name "GlobalProtect" | Out-Null
+}
+if (-not (Test-Path "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\PanSetup")) {
+  New-Item -Path "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect" -Name "PanSetup" | Out-Null
+}
+New-ItemProperty -LiteralPath "HKLM:\SOFTWARE\Palo Alto Networks\GlobalProtect\PanSetup" -Name "Portal" -Value "vpn.company.com" -PropertyType String
+
